70fd65ee4d9bbece65e3b8ccf4518d8a38489681
denizparlak1 <60672820+denizparlak1@users.noreply.github.com>
2021-12-20 13:26:15 +0300

    Test

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
+@app.route("/test")
+def hello_world2():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
